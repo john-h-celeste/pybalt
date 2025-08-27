a2586771942eb7cb99d56e1a588c127f0a61ad50
john-h-celeste <rvail8330@floridapoly.edu>
2025-08-27 15:14:42 +0000

    day04-02
    bodgodis binmterd

---
+++ day04.py
@@ -0,0 +1,20 @@
+# control structures
+
+
+
+
+# im going insane
+
+for i in range(13):
+    print('a')
+
+if 'photos printed':
+    print('bogos binted')
+
+nujiniuhyun = 0 <3
+but = lambda *_: calculus is better
+real = 'numbers'
+calculus = real and 'imaginary'
+better = 'than' [0]
+aaa = 'a' * b'a'[0]
+print(aaa [: but(real ): ])
